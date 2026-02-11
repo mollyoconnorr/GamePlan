708deb38d89b88590666e90b8df08635c923bc64
CanFam23 <nickclouse03@gmail.com>
2026-02-11 16:32:35 -0700

    Update vite config to include proxy for backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,32 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+    plugins: [react(),tailwindcss()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
+            },
+            // these are spring security endpoints involved in oauth2 login/logout
+            "/oauth2": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
+            },
+            "/login": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
+            },
+            "/logout": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+});
