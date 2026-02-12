e478b6c26707bff5584864aa1fb86bd7a2bac2a5
CanFam23 <nickclouse03@gmail.com>
2026-02-12 12:16:10 -0700

    Update health controller to use /api route

--- backend/src/main/java/com/carroll/gameplan/controller/HealthController.java
+++ backend/src/main/java/com/carroll/gameplan/controller/HealthController.java
@@ -3,12 +3,14 @@ package com.carroll.gameplan.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class HealthController {
     private final Logger logger = LoggerFactory.getLogger(HealthController.class);
 
