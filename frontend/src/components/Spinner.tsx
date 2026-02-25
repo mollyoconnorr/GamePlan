ac934a9315187f7c75b296a87dd6ad2580eb0639
CanFam23 <nickclouse03@gmail.com>
2026-02-25 16:16:26 -0700

    Add Spinner to display when loading

---
+++ frontend/src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+function Spinner() {
+    return (
+        <div className="flex justify-center items-center">
+            <div className="h-8 w-8 border-4 border-gray-300 border-t-primary rounded-full animate-spin"></div>
+        </div>
+    );
+}
+
+export default Spinner;
