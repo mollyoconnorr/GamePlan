58d6f3b5090c3ac76ed60cb32a360e33f7780dcf
Molly O'Connor <moconnor@carroll.edu>
2026-03-15 22:01:57 -0600

    Updated Reservation Request

--- backend/src/main/java/com/carroll/gameplan/dto/ReservationRequest.java
+++ backend/src/main/java/com/carroll/gameplan/dto/ReservationRequest.java
@@ -23,7 +23,6 @@ public class ReservationRequest {
      * The end datetime of the reservation, as a string (ISO-8601 format).
      */
     private String end;
-
     /**
      * Gets the equipment ID.
      *
