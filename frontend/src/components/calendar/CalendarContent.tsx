98be0b852aea949bf76ce414d0223291d91874e2
CanFam23 <nickclouse03@gmail.com>
2026-03-17 19:42:01 -0600

    Fix calendar events from having the wrong size, was caused by html structure

--- frontend/src/components/calendar/CalendarContent.tsx
+++ frontend/src/components/calendar/CalendarContent.tsx
@@ -1,20 +1,18 @@
 import type {CalendarContentProps} from "./CalendarTypes.ts";
 import {type JSX} from "react";
+import type {CalendarEvent} from "../../types.ts";
+import dayjs from "dayjs";
 
 export default function CalendarContent(props: CalendarContentProps) {
     // Map column / day indexes to events to render in those columns
-    const dayEventMap: Map<number, JSX.Element[]> = new Map();
+    const dayEventMap: Map<number, CalendarEvent[]> = new Map();
 
     // Add events to their respective columns
     props.events.forEach((e) => {
-        // Time index
-        const startIndex = props.timeMap.get(e.startTime);
-        const endIndex = props.timeMap.get(e.endTime);
-
         // Column index (Each column is one day)
         const colIndex = props.dayMap.get(e.date);
 
-        if (startIndex === undefined || endIndex === undefined || colIndex === undefined) {
+        if (colIndex === undefined) {
             return null;
         }
 
@@ -22,37 +20,124 @@ export default function CalendarContent(props: CalendarContentProps) {
             dayEventMap.set(colIndex, []);
         }
 
-        // Add JSX to the days array
-        dayEventMap.get(colIndex)!.push(
-            <p
-                key={e.name}
-                className="relative bg-blue-900 border-1 w-full break-all p-1 text-xs lg:text-md"
-                style={{
-                    top: props.cellHeight * startIndex,
-                    height: props.cellHeight * (endIndex - startIndex),
-                }}
-            >
-                {e.name}
-            </p>
-        );
+        // Add event to the days array
+        dayEventMap.get(colIndex)!.push(e);
     });
+
+    // Holds event HTML
+    const dayEventMapHTML: Map<number, JSX.Element[]> = new Map();
+
+    dayEventMap.forEach((dayEvents, key) => {
+        // Sort events earliest to latest (Arbitrary date needed for converting to dayjs obj)
+        dayEvents.sort((a, b) =>
+            dayjs(`1970-01-01 ${a.endTime}`, "YYYY-MM-DD h:mm A").valueOf() -
+            dayjs(`1970-01-01 ${b.endTime}`, "YYYY-MM-DD h:mm A").valueOf()
+        );
+
+        // Group events into groups that contain overlapping events
+        let i = 0;
+        while (i < dayEvents.length) {
+            const overlapping: CalendarEvent[] = []
+            overlapping.push(dayEvents[i]);
+
+            // While event (i) endTime overlaps next events (i+1) startTime
+            while (i < dayEvents.length - 1 && dayjs(`1970-01-01 ${dayEvents[i].endTime}`, "YYYY-MM-DD h:mm A").valueOf() > dayjs(`1970-01-01 ${dayEvents[i + 1].startTime}`, "YYYY-MM-DD h:mm A").valueOf()) {
+                overlapping.push(dayEvents[i + 1]);
+                i++;
+            }
+            i++;
+
+            if (!dayEventMapHTML.has(key)) {
+                dayEventMapHTML.set(key, []);
+            }
+
+            // Precompute the events starting and ending index
+            // So we can compute the proper bounds for its container
+            const indexedEvents = overlapping
+                .map((e) => {
+                    const startIndex = props.timeMap.get(e.startTime);
+                    const endIndex = props.timeMap.get(e.endTime);
+
+                    if (startIndex === undefined || endIndex === undefined) {
+                        return null;
+                    }
+
+                    return {
+                        event: e,
+                        startIndex,
+                        endIndex,
+                    };
+                })
+                // Filter for non-null
+                .filter((e): e is {
+                    event: CalendarEvent;
+                    startIndex: number;
+                    endIndex: number;
+                } => e !== null);
+
+            if (indexedEvents.length === 0) {
+                continue;
+            }
+
+            // Start and end index of group / container
+            const groupStartIndex = Math.min(...indexedEvents.map((e) => e.startIndex));
+            const groupEndIndex = Math.max(...indexedEvents.map((e) => e.endIndex));
+
+            // Push group to days array (Key is col index, each col represents a day)
+            dayEventMapHTML.get(key)!.push(
+                <div
+                    className="absolute flex flex-row justify-between space-x-1 mx-1"
+                    style={{
+                        top: props.cellHeight * groupStartIndex,
+                        height: props.cellHeight * (groupEndIndex - groupStartIndex),
+                        left: 0,
+                        right: 0,
+                    }}
+                >
+                    {overlapping.map((e) => {
+                        // Time index
+                        const startIndex = props.timeMap.get(e.startTime);
+                        const endIndex = props.timeMap.get(e.endTime);
+
+                        if (startIndex === undefined || endIndex === undefined) {
+                            return null;
+                        }
+
+                        // Day card TODO change to a card component that can be clicked on
+                        // TODO add event ID
+                        return <p
+                            key={e.name + e.date + e.startTime}
+                            className="relative bg-blue-900 border-1 w-full break-all p-1 text-xs lg:text-md"
+                            style={{
+                                top: props.cellHeight * (startIndex - groupStartIndex),
+                                height: props.cellHeight * (endIndex - startIndex),
+                            }}
+                        >
+                            {e.name}
+                        </p>
+                    })}
+                </div>);
+        }
+    })
 
     return (
         <div className="absolute z-10  h-full opacity-60 flex justify-between"
-             style={{top: props.top,
+             style={{
+                 top: props.top,
                  left: props.left,
                  height: `${props.height}px`,
                  width: `${props.width}px`,
                  maxWidth: `${props.width}px`,
-                 }
+             }
              }
         >
             {/*The # of keys in dayEventMap == props.numDays, so loop through num days to
             render each days events
             */}
-            {Array.from({length:props.numDays}).map((_, i) => (
-                <div key={i} className="w-full px-1 flex flex-row space-x-2">
-                    {dayEventMap.get(i)}
+            {Array.from({length: props.numDays}).map((_, i) => (
+                <div key={i} className="w-full px-1 relative"
+                     style={{ height: props.cellHeight * 36 }}>
+                    {dayEventMapHTML.get(i)}
                 </div>
             ))}
 
