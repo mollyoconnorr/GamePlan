cb6a23ec214b94e634c6d4c6454798d4e5c24a27
Molly O'Connor <moconnor@carroll.edu>
2026-03-18 23:18:17 -0600

    Fixed path issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import RequireAuth from "./auth/RequireAuth";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import ManageReservations from "./pages/ManageReservations.tsx";
+import ManageReservations from "./components/ManageReservations.tsx";
 import ReserveEquipment from "./pages/ReserveEquipment.tsx";
 import Welcome from "./pages/Welcome.tsx";
 import Login from "./pages/Login.tsx";
