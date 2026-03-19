02587cfeba5b10448d11e8014aa7bce9d055c735
CanFam23 <nickclouse03@gmail.com>
2026-03-18 20:32:25 -0600

    Combine parsing functions into one file, add function to parse RawRes to Event

--- frontend/src/util/ParseReservation.ts
+++ frontend/src/util/ParseReservation.ts
@@ -1,15 +1,25 @@
-import type {RawReservation} from "../types.ts";
+import type {CalendarEvent, RawReservation, Reservation} from "../types.ts";
 import dayjs from "dayjs";
 
-export function parseRawResToEvent(reservation: RawReservation ) {
-    const startDate = dayjs(reservation.start);
-    const endDate = dayjs(reservation.end);
+export function parseRawResToRes(reservation: RawReservation): Reservation {
+    return {
+        start: dayjs(reservation.start),
+        end: dayjs(reservation.end),
+        name: reservation.equipmentName,
+        id: reservation.id,
+    };
+}
 
+export function parseResToEvent(reservation: Reservation): CalendarEvent {
     return {
         id: reservation.id,
-        startTime: startDate.format("h:mm A"),
-        endTime: endDate.format("h:mm A"),
-        name: reservation.equipmentName,
-        date: startDate.format("ddd M/D"),
-    }
+        startTime: reservation.start.format("h:mm A"),
+        endTime: reservation.end.format("h:mm A"),
+        name: reservation.name,
+        date: reservation.start.format("ddd M/D"),
+    };
+}
+
+export function parseRawResToEvent(reservation: RawReservation): CalendarEvent {
+    return parseResToEvent(parseRawResToRes(reservation));
 }
