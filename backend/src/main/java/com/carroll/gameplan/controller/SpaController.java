fe5e8ee82178e751efb93a931eb48723f77064da
CanFam23 <nickclouse03@gmail.com>
2026-03-31 15:39:39 -0600

    Create SPA controller

--- backend/src/main/java/com/carroll/gameplan/controller/SpaController.java
+++ backend/src/main/java/com/carroll/gameplan/controller/SpaController.java
@@ -1,4 +1,18 @@
 package com.carroll.gameplan.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * This controller is used when the frontend is served as a static resource.
+ * Any frontend route (/app/**) is forwarded to index.html, since React handles
+ * routing on the client side.
+ */
+@Controller
 public class SpaController {
+
+    @GetMapping({"/app", "/app/{path:[^.]*}", "/app/{path:^(?!api$).*$}/**"})
+    public String forward() {
+        return "forward:/index.html";
+    }
 }
